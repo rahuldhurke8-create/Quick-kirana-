5a719eda44f1efdcc4e73a306dccafb62f023c42
rahuldhurke8-create <rahuldhurke8@gmail.com>
2026-04-03 14:01:27 +0530

    Create authController.js with user authentication functions

---
+++ src/controllers/authController.js
@@ -0,0 +1,24 @@
+// authController.js
+
+// Function to register a user
+function register(user) {
+    // Registration logic here
+}
+
+// Function to log in a user
+function login(credentials) {
+    // Login logic here
+}
+
+// Function to log out a user
+function logout() {
+    // Logout logic here
+}
+
+// Function to get the current user
+function getCurrentUser() {
+    // Logic to get current user details
+}
+
+// Export functions for use in other modules
+module.exports = { register, login, logout, getCurrentUser };
